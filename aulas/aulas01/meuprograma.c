880158701f484654e2052b4005ddf4aa30eb2abd
Thegitsucess123 <109971885+Thegitsucess123@users.noreply.github.com>
2023-08-24 00:48:28 +0000

    arquivos da aula 01

---
+++ aulas/aulas01/meuprograma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+printf("Olá Pessoal!\n");
+printf("A raiz quadrada de 2 é %f\n", sqrt(4));
+  
+return 0;
+}
+/*            
+Autor:André Rodrigo
+Data:23/08/2023
+
+Programa que exibe: Olá Pessoal!
+*/  
+// meu comentario
+// funcao principal
