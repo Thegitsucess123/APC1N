d0c00bb6086dfac5a8965e006b30a34267ce3391
Thegitsucess123 <109971885+Thegitsucess123@users.noreply.github.com>
2023-09-04 13:35:33 +0000

    Aula e Pratica 02

---
+++ .ccls-cache/@home@runner@APC1N/praticas@praticas01@questao2.c
@@ -0,0 +1,11 @@
+// Faça um programa em C que calcule a área de um triângulo (a = base x altura / 2). Considere base e altura números inteiros maior que 0.
+#include <stdio.h>
+#include <math.ar.h>
+int main(void){
+ 
+ int base = 6 ;
+ int altura = 4;
+ int area = base * altura /2;
+printf("A area do triangulo é %i\n", area);
+  
+}
